a86e59f293b94a14cac1a65760f57ab13b7dfab4
Jordán Dávid Nikolov <nikolov.jd@gmail.com>
2020-06-18 15:17:23 +0200

    fix: missing validation in management API

--- backend/src/restApi/route/management.js
+++ backend/src/restApi/route/management.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
 import management from '../controller/management'
+import project from "../controller/project";
 
 // GET request for list of all items
 router.get('/', management.list)
-router.post('/add', management.add)
-router.post('/remove', management.remove)
+router.post('/add', management.validationRules('add'), management.validate, management.add)
+router.post('/remove', management.validationRules('remove'), management.validate, management.remove)
 export default router
